5b921b5f3d00d5315223c5ac704629f746af2a61
xiagn17 <xiagn17@gmail.com>
2018-05-30 14:34:47 +0300

    Changed with included methods for work with date

--- task2.js
+++ task2.js
@@ -1,26 +1,16 @@
-var day = parseInt(process.argv[2]);
-var monthInString = process.argv[3];
-var year = parseInt(process.argv[4]);
+let day = parseInt(process.argv[2]);
+let monthInString = process.argv[3];
+let year = parseInt(process.argv[4]);
 
-var february = 28;
-if (year % 4 === 0) {
-    february = 29;
-}
-var daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var monthes = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 
-var month = -1;
-for (var i = 0; i < 12; i++){
-    if (monthes[i] === monthInString){
-        month = i;
-    }
-}
+let monthes = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 
-var sum = 0;
-for (var i = month + 1; i < 12; i++){
-    sum += daysInMonth[i];
-}
+let monthInInt = monthes.indexOf(monthInString);
 
-sum += daysInMonth[month] - day;
+let dateNow = new Date(year, monthInInt, day);
+let dateNextYear = new Date(year + 1, 0, 0);
 
-process.stdout.write(String(sum));
+let differense = dateNextYear.getTime() - dateNow.getTime();
+differense = Math.floor(differense / 86400000);
+
+process.stdout.write(String(differense));
