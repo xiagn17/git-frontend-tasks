628215b7ca788a418267a0716604ef96766e4e85
xiagn17 <xiagn17@gmail.com>
2018-06-13 18:43:22 +0300

    1more try

--- task2.js
+++ task2.js
@@ -6,17 +6,13 @@ let year = parseInt(process.argv[4]);
 let monthes = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
 let monthInInt = monthes.indexOf(monthInString);
 
-let daysInFebruary = 28;
-if (year % 4 === 0){
-    daysInFebruary = 29;
-}
-let daysInMonth = [31, daysInFebruary, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 let sum = 0;
-for (let i = monthInInt + 1; i < 12; i++){
+for (let i = monthInInt; i < 12; i++){
     sum += daysInMonth[i];
 }
 
-sum += daysInMonth[monthInInt] - day;
+sum -= day;
 
 process.stdout.write(sum.toString());
